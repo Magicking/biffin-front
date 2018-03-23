c3e30fed6d8d4d296d1f9eb4aee63f90210e6554
Unknown <lbabeanu@gmail.Com>
2018-03-23 12:42:19 +0100

    Took weirdness out

--- MainMenu.js
+++ MainMenu.js
@@ -1,23 +1,22 @@
 class MainMenu extends Phaser.Scene{
-	constructor() {
-		super({key:'MainMenu'});
-	}
-
-	preload(){
-		
+  constructor() {
+    super({key:'MainMenu'});
+  }
+ 
+  preload(){
+    
     this.load.atlas('menuButtons', 'assets/menuButtons.png', 'assets/menuButtons.json');
-	}
-
-	create(){
-		//Create Editor Button by grabbing image from set
-		var menuButton = this.add.sprite(width/2,height/2,'menuButtons','editorb2.png')
-		menuButton.setInteractive();
-		menuButton.on('pointerdown', function(pointer){
-			this.scene.start('Editor')
-		},this)
-<<<<<<< HEAD
-		//Some basic text to show we're awesome and show version
-   		 var text = this.make.text({
+  }
+ 
+  create(){
+    //Create Editor Button by grabbing image from set
+    var menuButton = this.add.sprite(width/2,height/2,'menuButtons','editorb2.png')
+    menuButton.setInteractive();
+    menuButton.on('pointerdown', function(pointer){
+      this.scene.start('Editor')
+    },this)
+    //Some basic text to show we're awesome and show version
+        var text = this.make.text({
         x: width-width+80,
         y: height-height+20,
         text: 'Biffin Editor refactor 0.01',
@@ -28,8 +27,6 @@ class MainMenu extends Phaser.Scene{
             fill: 'white',
         }
        })
-=======
->>>>>>> master
-
-	}
+ 
+  }
 }
